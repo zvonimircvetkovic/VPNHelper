8bf72b6530cb7cc46b0467eb32cbfa0eb0c8d0b0
Zvonimir Cvetkovic <zvonimir.cvetkovic@outlook.com>
2021-03-17 19:01:01 +0100

    project setup

---
+++ VPNHelperLibrary/DataAccess.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VPNHelperLibrary.Models;
+
+namespace VPNHelperLibrary
+{
+    public class DataAccess
+    {
+        IEnumerable<CountryModel> Countries = new List<CountryModel>
+        { 
+            new CountryModel { Name = "Croatia", Abrv = "cro" },
+            new CountryModel { Name = "US", Abrv = "us" },
+            new CountryModel { Name = "Netherlands", Abrv = "nl" }
+        };
+
+        public Task<IEnumerable<CountryModel>> GetCountriesAsync()
+        {
+            return Task.FromResult(Countries);
+        }
+    }
+}
---
+++ VPNHelperUI/ViewModels/ShellViewModel.cs
@@ -0,0 +1,47 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using VPNHelperLibrary;
+using VPNHelperLibrary.Models;
+
+namespace VPNHelperUI.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+        public BindableCollection<CountryModel> Countries { get; set; }
+
+        private CountryModel selectedCountry;
+
+        public ShellViewModel()
+        {
+            DataAccess da = new DataAccess();
+            Countries = new BindableCollection<CountryModel>(da.GetCountriesAsync().Result);
+        }
+
+        public CountryModel SelectedCountry 
+        { 
+            get { return selectedCountry; } 
+            set 
+            {
+                selectedCountry = value;
+                NotifyOfPropertyChange(() => SelectedCountry);
+            } 
+        }
+
+        public void Search()
+        {
+            if (SelectedCountry != null)
+            {
+                MessageBox.Show(SelectedCountry.Name);
+            }
+            else
+            {
+                MessageBox.Show("Please select a country.");
+            }
+        }
+    }
+}
