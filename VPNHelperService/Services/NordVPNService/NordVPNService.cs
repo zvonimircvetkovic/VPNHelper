5b9388ab58067fac677acdabddf09e2d51dc5ef0
Zvonimir Cvetković <zvonimir.cvetkovic@outlook.com>
2021-03-27 20:38:03 +0100

    added ping retreive

--- VPNHelperService/Services/NordVPNService/NordVPNService.cs
+++ VPNHelperService/Services/NordVPNService/NordVPNService.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net.NetworkInformation;
 using System.Text;
 using System.Threading.Tasks;
 using VPNHelperCommon.Clients;
@@ -58,11 +59,40 @@ namespace VPNHelperService.Services
                 worksheet.Cells[row, 1].Value = "Server";
                 worksheet.Cells[row, 2].Value = "Load";
                 worksheet.Cells[row, 3].Value = "IP";
+                worksheet.Cells[row, 4].Value = "Ping";
 
                 row++;
 
+                Ping p = new Ping();
+
                 foreach (var server in servers.Where(x => x.Load < 50).Take(1000))
                 {
+                    var ping = string.Empty;
+                    try
+                    {
+                        long total = 0;
+                        for (int i = 0; i < 5; i++)
+                        {
+                            PingReply reply = p.Send(server.IPAddress, 1000);
+                            if (reply != null)
+                            {
+                                total += reply.RoundtripTime;
+                            }
+                        }
+                        if (total > 0)
+                        {
+                            ping = $"{total / 5}ms";
+                        }
+                        else
+                        {
+                            ping = "Couldn't connect.";
+                        }
+                    }
+                    catch
+                    {
+                        ping = "Couldn't connect.";
+                    }
+
                     var column = 1;
                     worksheet.Cells[row, column].Value = server.Domain;
                     column++;
@@ -70,10 +100,12 @@ namespace VPNHelperService.Services
                     column++;
                     worksheet.Cells[row, column].Value = String.Format("{0} {1}", server.IPAddress, server.Features.OpenVPNTcp ? "443" : "1194");
                     column++;
-                    worksheet.Cells[row, column].Value = server.Features.OpenVPNTcp ? "TCP" : "UDP";
+                    worksheet.Cells[row, column].Value = ping;
 
                     row++;
                 }
+
+                p.Dispose();
 
                 await package.SaveAsync();
             }
